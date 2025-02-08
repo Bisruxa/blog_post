8823bf4a6b73584b3d52883b71315d37231945ef
Elkan <Elkan@GALAXY-BOOK>
2025-02-08 00:29:34 -0900

    allowed host changed

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^%5%#vf@7j-scegn9)y=lqdwc@%7go@jsk+d0j@y=(wg!zqa_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-post-sbr9.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
